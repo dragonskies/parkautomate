9a83a6355e829320e5dca0198ad01f972f560969
dragonskies <cpuobsessed@gmail.com>
2016-06-08 16:57:30 -0400

    fixed IE problems with links
    Check links is mostly working
    added password field

--- CheckLinks.java
+++ CheckLinks.java
@@ -1,6 +1,8 @@
 package parkrdu;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -14,6 +16,7 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
 import org.apache.commons.io.*;
 
@@ -35,8 +38,16 @@ public class CheckLinks {
   private String ccCSV;
   private String screenshotName = "screenshot";
   private Integer screenshotNumber = 0;
-  
-  public CheckLinks(Properties testdata, String browser){
+  private String browserName;
+ 
+  /**
+   * browser is sent from TestParameters
+   * browserName is used by CheckLinks
+   * 
+   * @param testdata
+   * @param browser
+   */
+  public CheckLinks(Properties testdata,  String browser){
       entrydate = testdata.getProperty("entrydate");
       exitdate = testdata.getProperty("exitdate");
       ccName = testdata.getProperty("ccName");
@@ -48,51 +59,77 @@ public class CheckLinks {
       ccMonth = testdata.getProperty("ccmonth");
       if (browser == "Firefox"){
           driver = new FirefoxDriver();
+          browserName = "FF";
       } else if (browser == "Internet Explorer 11"){
-          //System.setProperty("", "baseUrl");
-          driver = new InternetExplorerDriver();
+    	  DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer(); 
+    	  ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+    	  ieCapabilities.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL, "https://rdu-booking.preprod.inventiveit.net/en/");
+          driver = new InternetExplorerDriver(ieCapabilities);
+          browserName = browser;
+          try {
+			Thread.sleep(400);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
       } else if (browser == "Chrome"){
-          System.setProperty("webdriver.chrome.driver", "C:\\Users\\davist\\Downloads\\chromedriver_win32(2)\\chromedriver.exe");
+    	  URL url = this.getClass().getClassLoader().getResource("chromedriver.exe");
+    	  System.setProperty("webdriver.chrome.driver", url.getPath());
           driver = new ChromeDriver();
-      } else if (browser == "Safari"){
-          driver = new SafariDriver();
+          browserName = browser;
       } else if (browser == "Edge"){
-          System.setProperty("webdriver.edge.driver", "C:\\Users\\davist\\Downloads\\MicrosoftWebDriver.exe");
+    	  URL url = this.getClass().getClassLoader().getResource("MicrosoftWebDriver.exe");
+          System.setProperty("webdriver.edge.driver", url.getPath());
           driver = new EdgeDriver();
+          browserName = browser;
       } else if (browser == "Opera"){
           driver = new OperaDriver();
+          browserName = browser;
       }
     baseUrl = "https://rdu-booking.preprod.inventiveit.net/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS );
+    driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS );
   }
 
   public void testCheckLinks() throws Exception {
 
     driver.get(baseUrl + "/en/");
-    String parentWindow = driver.getWindowHandle();
-    driver.findElement(By.cssSelector("header-logo")).click();
-    driver.findElement(By.xpath("html/body/main/nav/div/div[1]/div/ul/li[1]/a")).click();
+    Thread.sleep(20000);
+
+    //special code to click the rdu logo
+    WebElement rduLogo = driver.findElement(By.className("header-logo"));
+    rduLogo.findElement(By.tagName("img")).click();
+    // 
+    //driver.findElement(By.cssSelector("a[href*=/en/")).click();
+    Thread.sleep(2000);
+    //String parentWindow = driver.getWindowHandle();
     File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     screenshotNumber += 1;
     FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
+    
     driver.findElement(By.xpath("//nav[@class='header-bottom']//a[.='Parking Information']")).click();
+    Thread.sleep(200);
     screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     screenshotNumber += 1;
     FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     driver.findElement(By.xpath("//nav[@class='header-bottom']//a[.='Manage My Booking']")).click();
+    Thread.sleep(200);
     screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     screenshotNumber += 1;
     FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     driver.findElement(By.xpath("//nav[@class='header-bottom']//a[.='Login']")).click();
+    Thread.sleep(200);
     screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     screenshotNumber += 1;
     FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     driver.findElement(By.xpath("//nav[@class='header-bottom']//a[.='Create Account']")).click();
+    Thread.sleep(200);
     screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     screenshotNumber += 1;
     FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     driver.findElement(By.linkText("TERMS & CONDITIONS")).click();
+    Thread.sleep(200);
+    String parentWindow = driver.getWindowHandle();
     Set<String> handles = driver.getWindowHandles();
     	for (String windowHandle : handles){
     		if(!windowHandle.equals(parentWindow)){
@@ -101,16 +138,19 @@ public class CheckLinks {
     		    screenshotNumber += 1;
     		    FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     		    driver.findElement(By.linkText("PRIVACY POLICY")).click();
+    		    Thread.sleep(200);
     		    //driver.switchTo().window("Privacy Policy | RDU International Airport");
     		    screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     		    screenshotNumber += 1;
     		    FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     		    driver.findElement(By.linkText("CONTACT US")).click();
+    		    Thread.sleep(200);
     		    //driver.switchTo().window("Contact us | RDU International Airport");
     		    screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     		    screenshotNumber += 1;
     		    FileUtils.copyFile(screenshot, new File(screenshotName+screenshotNumber.toString()+".png"));
     		    driver.findElement(By.xpath("(//a[contains(text(),'Frequently Asked Questions')])[2]")).click();
+    		    Thread.sleep(200);
     		    //driver.switchTo().window("Frequently Asked Questions | RDU International Airport");
     		    screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
     		    screenshotNumber += 1;
